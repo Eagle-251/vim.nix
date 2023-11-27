3c613c790bf1c0f339861eced0a05ae893dce235
Ewan Gilchrist <ewan@gilchrist.scot>
2023-11-27 16:04:36 +0100

    Add ansiblels autocommands

--- config/settings/autocommands.nix
+++ config/settings/autocommands.nix
@@ -12,5 +12,20 @@
       pattern = [ "*" ];
       command = "if &nu | set nornu | endif";
     }
+    {
+      event = [ "BufLeave" "BufNewFile"];
+    	pattern = ["*/playbooks/*.yml"];
+    	command = "set filetype=yaml.ansible";
+    }
+    {
+      event = [ "BufLeave" "BufNewFile"];
+	    pattern = ["*/roles/*.yml"];
+	    command = "set filetype=yaml.ansible";
+    }
+    {
+      event = [ "BufLeave" "BufNewFile"];
+	    pattern = ["*/inventory/*.yml"];
+	    command = "set filetype=yaml.ansible";
+    }
   ];
 }
