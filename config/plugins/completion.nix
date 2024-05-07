77177ce2adbd8a877b253a1bf9190f0374add01f
Ewan Gilchrist <ewan@gilchrist.scot>
2024-05-07 15:36:37 +0200

    Fix syntax

--- config/plugins/completion.nix
+++ config/plugins/completion.nix
@@ -13,20 +13,20 @@
           {name = "buffer";}
           {name = "treesitter";}
         ];
-      };
-      mapping = {
-        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
-        "<C-Space>" = "cmp.mapping.complete()";
-        "<C-e>" = "cmp.mapping.abort()";
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = {
-          action = "cmp.mapping.select_next_item()";
-          modes = ["i" "s"];
-        };
-        "<S-Tab>" = {
-          action = "cmp.mapping.select_prev_item()";
-          modes = ["i" "s"];
+        mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            action = "cmp.mapping.select_next_item()";
+            modes = ["i" "s"];
+          };
+          "<S-Tab>" = {
+            action = "cmp.mapping.select_prev_item()";
+            modes = ["i" "s"];
+          };
         };
       };
     };
