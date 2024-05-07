e7115ddf36af209ca2e26ab0c15e10696d5a19fc
Ewan Gilchrist <ewan@gilchrist.scot>
2024-05-07 15:33:55 +0200

    Revert changes to nvim-cmp... it's simpler than I thought..

--- config/plugins/completion.nix
+++ config/plugins/completion.nix
@@ -2,12 +2,39 @@
 
 {
   plugins = {
-    cmp.enable = true;
-    cmp-rg.enable = true;
-    cmp-path.enable = true;
-    cmp-nvim-lsp.enable = true;
-    cmp_luasnip.enable = true;
-    cmp-buffer.enable = true;
-    cmp-treesitter.enable = true;
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [
+          {name = "path";}
+          {name = "nvim_lsp";}
+          {name = "luasnip";}
+          {name = "buffer";}
+          {name = "treesitter";}
+        ];
+      };
+      mapping = {
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-e>" = "cmp.mapping.abort()";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<Tab>" = {
+          action = "cmp.mapping.select_next_item()";
+          modes = ["i" "s"];
+        };
+        "<S-Tab>" = {
+          action = "cmp.mapping.select_prev_item()";
+          modes = ["i" "s"];
+        };
+      };
+    };
+   # cmp-rg.enable = true;
+   # cmp-path.enable = true;
+   # cmp-nvim-lsp.enable = true;
+   # cmp_luasnip.enable = true;
+   # cmp-buffer.enable = true;
+   # cmp-treesitter.enable = true;
   };
 }
