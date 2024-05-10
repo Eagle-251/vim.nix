7570608dddead60c117e809fcd2c21467b8bbbbe
Ewan Gilchrist <ewan@gilchrist.scot>
2024-05-10 14:27:36 +0200

    Remove custom neogit views

--- config/plugins/neogit.nix
+++ config/plugins/neogit.nix
@@ -2,22 +2,22 @@
 
 {
   plugins.neogit.settings = {
-    commit_editor = {
-      kind = "replace";
-    };
-    commit_popup = {
-      kind = "floating";
-    };
-    integrations = {
-      diffview = true;
-    };
-    kind = "floating";
-    popup = {
-      kind = "floating";
-    };
-    preview_buffer = {
-      kind = "floating";
-    };
+#    commit_editor = {
+#      kind = "replace";
+#    };
+#    commit_popup = {
+#      kind = "floating";
+#    };
+#    integrations = {
+#      diffview = true;
+#    };
+#    kind = "floating";
+#    popup = {
+#      kind = "floating";
+#    };
+#    preview_buffer = {
+#      kind = "floating";
+#    };
    # sections = {
    #   recent = {
    #     folded = true;
